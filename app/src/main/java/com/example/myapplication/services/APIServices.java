96dd8bab1b8e00067a13f9a851d6dd3363199db3
conchonha <thaibasang8@gmail.com>
2021-05-26 14:32:43 +0700

    done

--- app/src/main/java/com/example/myapplication/services/APIServices.java
+++ app/src/main/java/com/example/myapplication/services/APIServices.java
@@ -4,7 +4,8 @@ public class APIServices {
     private static String baseurl="http://192.168.1.10/AppDuLich/public/";
 
     public static DataService getService(){
-
+    //Pager Adapter
+        //CoodinatorLayout
         return APIRetrofitClient.getClient(baseurl).create(DataService.class);
     }
 }
